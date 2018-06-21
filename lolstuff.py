9e21663cd63cae329676a3d82559d587fa49bb4c
CHARRAT pierre <pierre.charrat@teamwork.net>
2018-06-21 09:02:05 +0200

    add slack interaction + matchs

--- lolstuff.py
+++ lolstuff.py
@@ -6,6 +6,7 @@ import urllib.parse
 from elasticsearch import Elasticsearch, RequestsHttpConnection, TransportError
 from elasticsearch import RequestError
 # from aws_requests_auth.aws_auth import AWSRequestsAuth
+slack_data = {}
 
 INDEX_MAPPING = {
     "mappings": {
@@ -24,10 +25,25 @@ INDEX_MAPPING = {
 
 def main():
     conf =""
+    allSummonerIDs  =[]
     with open('conf.json') as f:
         conf = json.load(f)
-    summonerID = getSummoner(conf)
-    print(summonerID)
+    for invocateur in conf["lol"]["summonerName"]:
+        summonerID = getSummoner(invocateur,conf)
+        print(summonerID)
+        allSummonerIDs.append(summonerID)
+    for summ in allSummonerIDs:
+        myMatchs=getMatchs(summ["accountId"],conf)
+        for match in myMatchs["matches"]:
+            participants = ""
+            selectedMatch = getMatchInfos(match["gameId"],conf)
+            for player in selectedMatch["participantIdentities"]:
+                print(player)
+                participants += player["player"]["summonerName"] +", "
+            print(selectedMatch["teams"])
+            slack_data['text']="you played with: "+participants
+            resp = requests.post(conf["slack"]["webhookURL"], data=json.dumps(slack_data), headers={'Content-Type': 'application/json'})
+            break
     # es_client = Elasticsearch(
     #                 host="localhost",
     #                 port=9200,
@@ -51,12 +67,20 @@ def create_index(es_client, index):
         logger.info("Check ElasticSearch instances, must not be viable or healthy")
 
 
+def getMatchInfos(matchID,conf):
+    reconstructUrl ="https://"+conf["lol"]["server"]+conf["lol"]["baseURL"]+conf["lol"]["matchURL"]+str(matchID)+"?api_key="+conf["lol"]["apiKey"]
+    req = requests.get(reconstructUrl)
+    match = json.loads(req.content)
+    return match
 
-def getMatchs(conf):
+def getMatchs(summonerID,conf):
     reconstructUrl ="https://"+conf["lol"]["server"]+conf["lol"]["baseURL"]+conf["lol"]["matchListURL"]+str(summonerID)+"?api_key="+conf["lol"]["apiKey"]
+    req = requests.get(reconstructUrl)
+    matchList = json.loads(req.content)
+    return matchList
 
-def getSummoner(conf):
-    summoner=conf["lol"]["summonerName"].replace(" ", "%20")
+def getSummoner(invocateur,conf):
+    summoner=invocateur.replace(" ", "%20")
     print("pouet")
     reconstructUrl ="https://"+conf["lol"]["server"]+conf["lol"]["baseURL"]+conf["lol"]["summonerURL"]+summoner+"?api_key="+conf["lol"]["apiKey"]
     # reconstructUrl = "toto"
